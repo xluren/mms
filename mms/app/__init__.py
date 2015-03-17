70d36afd3b461b2686ed546065888390ff85304e
xluren <xluren@126.com>
2015-03-17 23:53:21 +0800

    first step

--- mms/app/__init__.py
+++ mms/app/__init__.py
@@ -1,21 +1,12 @@
 from flask import Flask
-import logging
-from logging.handlers import RotatingFileHandler
-
-
 
 app = Flask(__name__)
+
 app.config.from_object("config")
 
-'''register_blueprint'''
-from app.module_one.views import module_one
-from app.module_two.views import module_two
-app.register_blueprint(module_one, url_prefix='/module_one')
-app.register_blueprint(module_two, url_prefix='/module_two')
-print app.url_map
+from app.module_machine.machine_api import machine_api 
 
-'''use logging'''
-handler = RotatingFileHandler('foo_bar.log', maxBytes=10000, backupCount=1)
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
+app.register_blueprint(machine_api,url_prefix='/machine_api')
+
+print app.url_map
 
--- mms/config.py
+++ mms/config.py
@@ -1,10 +1,11 @@
-DEBUG = True
-
-import os,sys
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-#DATABASE_CONNECT_OPTIONS = {}
-CSRF_ENABLED     = True
-CSRF_SESSION_KEY = "secret"
-SECRET_KEY = "secret"
-LOG_PATH=sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"/log")))
+class Config(object):
+	DEBUG=False
+	SQLALCHEMY_DATABASE_URI='sqlite:////tmp/sql_test.db'
+	CSRF_ENABLED=True
+	CSRF_SESSION_KEY="secret"
+	SECRER_KEY="hello secret key"
+class MysqlConfig(Config):
+	SQLALCHEMY_DATABASE_URI='mysql://hello:hello@10.210.71.145:3306/sqlalchemy'
+	DEBUG=True
+class TestConfig(Config):
+	DEBUG=True
