ca95364f762611b3c656567bfcc1c506f7dc13e5
xluren <xluren@126.com>
2015-04-02 23:38:05 +0800

    initial db model

--- mms/app/common/db_model.py
+++ mms/app/common/db_model.py
@@ -2,33 +2,34 @@ from app import db
 from passlib.apps import custom_app_context as password_context
 
 class Service(db.Model):
-    __tablename__   = "service_table"
 
-    service_id      = db.Column(db.Integer,primary_key=True,index=True)
-    service_name    = db.Column(db.String(20))
-    service_fid     = db.Column(db.Integer)
-    group_id        = db.Column(db.Integer)
-    owner_ids       = db.Column(db.String(28))
+    __tablename__   = "service"
 
-    def __init__(self,service_name,service_fid,group_id,owner_ids):
-        self.service_name   = service_name
-        self.service_fid    = service_fid
-        self.group_id       = group_id
-        self.owner_ids      = owner_ids
+    id      = db.Column(db.Integer,primary_key=True,index=True)
+    name    = db.Column(db.String(20))
+    fid     = db.Column(db.Integer)
+
+    def __init__(self,name,fid):
+        self.name   = name
+        self.fid    = fid
     def __repr__(self):
-        return "<service info %r,%r,%r>" % (self.service_id,self.service_name,self.servic_fid)
+        return "<service info %r,%r,%r>" % (self.id,self.name,self.fid)
 
 class User(db.Model):
-    __tablename__ = 'user_table'
-    user_id         = db.Column(db.Integer, primary_key=True)
-    user_name       = db.Column(db.String(32), index=True,unique=True)
-    user_password   = db.Column(db.String(64))
-    user_groupid    = db.Column(db.Integer)
 
-    def __init__(self, name, password,groupid):
-        self.user_name      = username
-        self.user_groupid   = service_id
-        self.user_password  = self.hash_password(password)
+    __tablename__ = 'user'
+
+    id         = db.Column(db.Integer, primary_key=True)
+    name       = db.Column(db.String(32))
+    password   = db.Column(db.String(64))
+    serviceid  = db.Column(db.Integer)
+    permission = db.Column(db.String(5))
+
+    def __init__(self, name, password,serviceid,permission):
+        self.name       = name
+        self.password   = self.hash_password(password)
+        self.serviceid  = serviceid
+        self.permission = permission
 
     def hash_password(self, password):
         return password_context.encrypt(password)
@@ -39,9 +40,32 @@ class User(db.Model):
     def set_password(self, password):
         self.password=self.hash_password(password)
 
-    def set_user_name(self, user_name):
-        self.username=username
+class Host(db.Model):
 
-    def set_service_id(self,service_id):
-        self.service_id=service_id
+    __tablename__="host"
 
+    id          =   db.Column(db.Integer,primary_key=True)
+    hostname    =   db.Column(db.String(64))
+    hostip      =   db.Column(db.String(25))
+    kernal      =   db.Column(db.String(28))
+    description =   db.Column(db.String(28))
+    carrier     =   db.Column(db.String(15))
+    
+    cpus        =   db.Column(db.Integer)
+    rams        =   db.Column(db.Integer)
+    disk        =   db.Column(db.Integer)
+    
+    serviceids  =   db.Column(db.String(28))
+
+    def __init__(self,hostname,hostip,kernal,
+                    description,carrier,
+                    cpus,rams,disk,serviceids):
+        self.hostname       = hostname
+        self.hostip         = hostip 
+        self.kernal         = kernal 
+        self.description    = description
+        self.carrier        = carrier 
+        self.cpus           = cpus 
+        self.rams           = rams 
+        self.disk           = disk 
+        self.serviceids     = serviceids 
