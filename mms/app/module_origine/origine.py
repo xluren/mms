fefcc4b9a4079a71c7ade4d455ea0c51670fbd7b
xluren <xluren@126.com>
2015-04-07 21:38:16 +0800

    complete select module

--- mms/app/module_origine/origine.py
+++ mms/app/module_origine/origine.py
@@ -9,19 +9,30 @@ class origine(object):
         output_keys         = params["output"]
         filter_condition    = params["filter"]
 
-        sql=",".join(output_keys)
-        where=""
+        select_item=",".join(output_keys)
+        where_condition=""
 
         for key,value in  filter_condition.items():
-            where+='%s in ( "' % key +'","'.join(value)+'") and '
+            where_condition+='%s in ( "' % key +'","'.join(value)+'") and '
 
-        return "select %s from host where %s" %(sql,where[0:-4])
+
+        obj=__import__("app.common.db_model.models" ,fromlist=["app.common.db_model.models"])
+        cls=getattr(obj,table_name)
+        result=db.session.execute("select %s from %s where %s " %(select_item,table_name,where_condition[0:-4]))
+
+        result_list=[]
+
+        for item in result:
+            result_list.append(dict(zip(output_keys,item)))
+        
+        return json.dumps({"result":result_list},indent=2)
+
 
     def delete(self,table_name,params):
-        where=""
+        where_condition=""
 
         for key,value in  params.items():
-            where+='%s in ( "' % key +'","'.join(value)+'") and '
+            where_condition+='%s in ( "' % key +'","'.join(value)+'") and '
 
         return "delete from host where %s" %(where[0:-4])
 
@@ -33,7 +44,6 @@ class origine(object):
         return "update host"
 
     def insert(self,table_name,params):
-        from app.common.db_model.models import host
         obj=__import__("app.common.db_model.models" ,fromlist=["app.common.db_model.models"])
         cls=getattr(obj,table_name)
         print "#"
